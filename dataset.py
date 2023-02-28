7b6209442cf2f712b0d855272dd5e20e7f291da6
Arman <armanexplorer@gmail.com>
2023-02-28 17:23:50 +0330

    divan-e-shams dataset and its interface for reading added

--- dataset.py
+++ dataset.py
@@ -1,12 +1,17 @@
 import re
 from typing import List
 
+# 495 Hafez qazals
 # [\s\S*] has been used for covering first line unicdoe character ('\ufeff')
-qazal_pattern = re.compile(r'[\s\S*]غزل[\s+]{3,}[\S\s]+?\n')
-qazals_path = './datasets/hafez/hafez-qazals.txt'
+qazal_pattern = re.compile(r'\n?[\s\S*]غزل[\s+]{3,}[\S\s]+?\n\n')
+hafez_qazals_path = './datasets/hafez/hafez-qazals.txt'
 
+# 147 Poems (yek-beiti)
 poems_path = './datasets/poems/yek-beiti.txt'
 
+# 3229 Moulavi divan-e-shams qazals
+moulavi_qazals_pattern = re.compile(r'\n?\s*\d+\s*?\n')
+moulavi_qazals_path = './datasets/moulavi/divane-e-shams.txt'
 
 def read_qazals() -> List[str]:
     """Read Hafez qazals and return an array of his qazals.
@@ -14,20 +19,31 @@ def read_qazals() -> List[str]:
     Returns:
         List[str]: Array of qazals.
     """
-    with open(qazals_path, encoding='utf-8') as f:
-        lines = f.readlines()
-        from functools import reduce
-        text = reduce(lambda x,y: x + y, lines)
+    with open(hafez_qazals_path, encoding='utf-8') as f:
+        text = f.read()
         
     qazals_arr = qazal_pattern.split(text)
     return qazals_arr[1:]
 
 # test that the qazals have been read correctly
-# print(read_qazals()[200])
+# print(read_qazals()[494])
 
 def read_poems():
     with open(poems_path, encoding='utf-8') as f:
         poems = f.readlines()
         return poems
+
+
+# test that the poems have been read correctly
+# print(read_poems()[146])
+
+
+def read_divan_shams():
+    with open(moulavi_qazals_path, encoding='utf-8') as f:
+        text = f.read()
         
-print(read_poems()[10])
+    qazals_arr = moulavi_qazals_pattern.split(text)
+    return qazals_arr[1:]
+
+# test that the moulavi qazals have been read correctly
+# print(read_divan_shams()[3228])
